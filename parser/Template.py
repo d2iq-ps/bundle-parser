7b6825d79e5ce25397526819367a2c147a455558
swiftsuretech <dave@davewhitehouse.co.uk>
2023-01-26 18:18:16 +0000

    extracted template

---
+++ parser/Template.py
@@ -0,0 +1,93 @@
+template = {
+            "parse_time": "",
+            "customer_name": "",
+            "bundle_name": "",
+            "cluster_name": "",
+            "data": [
+                {
+                    "index": 0,
+                    "key": "Deployment Type",
+                    "value": "Deployed"
+                },
+                {
+                    "index": 1,
+                    "key": "Kubernetes Distribution",
+                    "value": ""
+                },
+                {
+                    "index": 2,
+                    "key": "DKP Distribution Version",
+                    "value": ""
+                },
+                {
+                    "index": 3,
+                    "key": "Kubernetes Version",
+                    "value": "Deployed"
+                },
+                {
+                    "index": 4,
+                    "key": "Infrastructure",
+                    "value": ""
+                },
+                {
+                    "index": 5,
+                    "key": "Operating System",
+                    "value": ""
+                },
+                {
+                    "index": 6,
+                    "key": "Operating System Version",
+                    "value": ""
+                },
+                {
+                    "index": 7,
+                    "key": "Number of Nodes",
+                    "value": ""
+                },
+                {
+                    "index": 8,
+                    "key": "Number of CPUs",
+                    "value": ""
+                },
+                {
+                    "index": 9,
+                    "key": "Number GPUs",
+                    "value": ""
+                },
+                {
+                    "index": 10,
+                    "key": "Air Gapped",
+                    "value": ""
+                },
+                {
+                    "index": 11,
+                    "key": "FIPS",
+                    "value": ""
+                },
+                {
+                    "index": 12,
+                    "key": "Catalogue Applications",
+                    "value": ""
+                },
+                {
+                    "index": 13,
+                    "key": "Management Cluster",
+                    "value": ""
+                },
+                {
+                    "index": 14,
+                    "key": "Management Cluster Version",
+                    "value": ""
+                },
+                {
+                    "index": 15,
+                    "key": "Total Number of Clusters Managed",
+                    "value": ""
+                },
+                {
+                    "index": 16,
+                    "key": "Notes",
+                    "value": ""
+                },
+            ]
+        }
